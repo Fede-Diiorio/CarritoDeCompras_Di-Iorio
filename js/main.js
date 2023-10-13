c3c5b7bb6306bcc53614650e8a7535f0034b5ae2
Federico Di Iorio <federico_metallica@hotmail.com>
2023-10-13 07:04:46 -0300

    Se actualizó el programa de JavaScript acorde al html.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
-// CODIGO DE JAVASCRIPT APLICADO PREVIO AL CURSO
-function toggleInput(inputId) {
-    var input = document.getElementById(inputId);
-    input.checked = !input.checked;
-}
-
 // FUNCIONES
 function contarProductos() {
-    productos = contador1 + contador2 + contador3 + contador4 + contador5;
+    productos = contador1 + contador2 + contador3 + contador4 + contador5 + contador6 + contador7 + contador8 + contador9;
     return productos;
 }
 
 function sumarTotal() {
-    total = (producto1 * contador1) + (producto2 * contador2) + (producto3 * contador3) + (producto4 * contador4) + (producto5 * contador5);
+    total = (producto1 * contador1) + (producto2 * contador2) + (producto3 * contador3) + (producto4 * contador4) + (producto5 * contador5) + (producto6 * contador6) + (producto7 * contador7) + (producto8 * contador8) + (producto9 * contador9);
     return total;
 }
 
 function agregarAlCarrito() {
-    let carrito = parseInt(prompt("Ingrese el número del producto que desea añadir al carrito:\n1. Producto1 = 1000 ==> " + contador1 + "\n2. Producto2 = 500 ==> " + contador2 + "\n3. Producto3 = 2000 ==> " + contador3 + "\n4. Producto4 = 750 ==> " + contador4 + "\n5. Producto5 = 1225 ==> " + contador5 + "\n0. Dejar de agregar productos"));
+    let carrito = parseInt(prompt("Ingrese el número del producto que desea añadir al carrito:\n1. Fuente Sentey 700W = $42000 ==> " + contador1 + "\n2. Memoria Ram 8gb = $13500 ==> " + contador2 + "\n3. Disco Rigido 2tb = $43500 ==> " + contador3 + "\n4. Webcam Philips = $12500 ==> " + contador4 + "\n5. Placa Madre Asrock = $160000 ==> " + contador5 + "\n6. Procesador Intel I7 = $165000 ==> " + contador6 + "\n7. Disco Solido Crucial 1tb = $32000 ==> " + contador7 + "\n8. Disco Solido ADATA 120gb = $9700 ==> " + contador8 + "\n9. Disco Solido Kingston 240gb = $13000 ==> " + contador9 + "\n0. Dejar de agregar productos"));
     while (carrito !== 0) {
         switch (carrito) {
             case 1:
@@ -34,17 +28,28 @@ function agregarAlCarrito() {
             case 5:
                 contador5++;
                 break;
+            case 6:
+                contador6++;
+                break;
+            case 7:
+                contador7++;
+                break;
+            case 8:
+                contador8++;
+                break;
+            case 9:
+                contador9++;
+                break;
             default:
                 alert("Número de producto no válido. Por favor, seleccione un número de producto válido.");
                 break;
         }
-        carrito = parseInt(prompt("Ingrese el número del producto que desea añadir al carrito:\n1. Producto1 = 1000 ==> " + contador1 + "\n2. Producto2 = 500 ==> " + contador2 + "\n3. Producto3 = 2000 ==> " + contador3 + "\n4. Producto4 = 750 ==> " + contador4 + "\n5. Producto5 = 1225 ==> " + contador5 + "\n0. Dejar de agregar productos"));
+        carrito = parseInt(prompt("Ingrese el número del producto que desea añadir al carrito:\n1. Fuente Sentey 700W = $42000 ==> " + contador1 + "\n2. Memoria Ram 8gb = $13500 ==> " + contador2 + "\n3. Disco Rigido 2tb = $43500 ==> " + contador3 + "\n4. Webcam Philips = $12500 ==> " + contador4 + "\n5. Placa Madre Asrock = $160000 ==> " + contador5 + "\n6. Procesador Intel I7 = $165000 ==> " + contador6 + "\n7. Disco Solido Crucial 1tb = $32000 ==> " + contador7 + "\n8. Disco Solido ADATA 120gb = $9700 ==> " + contador8 + "\n9. Disco Solido Kingston 240gb = $13000 ==> " + contador9 + "\n0. Dejar de agregar productos"));
     }
 }
 
 function quitarDelCarrito() {
-    let carrito = parseInt(prompt("Ingrese el número del producto que desea añadir al carrito:\n1. Producto1 = 1000 ==> " + contador1 + "\n2. Producto2 = 500 ==> " + contador2 + "\n3. Producto3 = 2000 ==> " + contador3 + "\n4. Producto4 = 750 ==> " + contador4 + "\n5. Producto5 = 1225 ==> " + contador5 + "\n0. Dejar de agregar productos"));
-
+    let carrito = parseInt(prompt("Ingrese el número del producto que desea quitar del carrito:\n1. Fuente Sentey 700W = $42000 ==> " + contador1 + "\n2. Memoria Ram 8gb = $13500 ==> " + contador2 + "\n3. Disco Rigido 2tb = $43500 ==> " + contador3 + "\n4. Webcam Philips = $12500 ==> " + contador4 + "\n5. Placa Madre Asrock = $160000 ==> " + contador5 + "\n6. Procesador Intel I7 = $165000 ==> " + contador6 + "\n7. Disco Solido Crucial 1tb = $32000 ==> " + contador7 + "\n8. Disco Solido ADATA 120gb = $9700 ==> " + contador8 + "\n9. Disco Solido Kingston 240gb = $13000 ==> " + contador9 + "\n0. Dejar de agregar productos"));
     while (carrito !== 0) {
         switch (carrito) {
             case 1:
@@ -72,16 +77,36 @@ function quitarDelCarrito() {
                     contador5--;
                 }
                 break;
+            case 6:
+                if (contador6 > 0) {
+                    contador6--;
+                }
+                break;
+            case 7:
+                if (contador7 > 0) {
+                    contador7--;
+                }
+                break;
+            case 8:
+                if (contador8 > 0) {
+                    contador8--;
+                }
+                break;
+            case 9:
+                if (contador9 > 0) {
+                    contador9--;
+                }
+                break;
             default:
                 alert("Número de producto no válido o no tiene ese producto agregado. Por favor, seleccione un número de producto válido.");
                 break;
         }
-        carrito = parseInt(prompt("Ingrese el número del producto que desea añadir al carrito:\n1. Producto1 = 1000 ==> " + contador1 + "\n2. Producto2 = 500 ==> " + contador2 + "\n3. Producto3 = 2000 ==> " + contador3 + "\n4. Producto4 = 750 ==> " + contador4 + "\n5. Producto5 = 1225 ==> " + contador5 + "\n0. Dejar de agregar productos"));
+        carrito = parseInt(prompt("Ingrese el número del producto que desea aquitar del carrito:\n1. Fuente Sentey 700W = $42000 ==> " + contador1 + "\n2. Memoria Ram 8gb = $13500 ==> " + contador2 + "\n3. Disco Rigido 2tb = $43500 ==> " + contador3 + "\n4. Webcam Philips = $12500 ==> " + contador4 + "\n5. Placa Madre Asrock = $160000 ==> " + contador5 + "\n6. Procesador Intel I7 = $165000 ==> " + contador6 + "\n7. Disco Solido Crucial 1tb = $32000 ==> " + contador7 + "\n8. Disco Solido ADATA 120gb = $9700 ==> " + contador8 + "\n9. Disco Solido Kingston 240gb = $13000 ==> " + contador9 + "\n0. Dejar de agregar productos"));
     }
 }
 
 function consultarCarrito() {
-    alert("1. Producto1 = 1000 ==> " + contador1 + "\n2. Producto2 = 500 ==> " + contador2 + "\n3. Producto3 = 2000 ==> " + contador3 + "\n4. Producto4 = 750 ==> " + contador4 + "\n5. Producto5 = 1225 ==> " + contador5 + "\nUsted tiene agregados " + contarProductos() + " productos al carrito sumando un total de $" + sumarTotal());
+    alert("1. Fuente Sentey 700W = $42000 ==> " + contador1 + "\n2. Memoria Ram 8gb = $13500 ==> " + contador2 + "\n3. Disco Rigido 2tb = $43500 ==> " + contador3 + "\n4. Webcam Philips = $12500 ==> " + contador4 + "\n5. Placa Madre Asrock = $160000 ==> " + contador5 + "\n6. Procesador Intel I7 = $165000 ==> " + contador6 + "\n7. Disco Solido Crucial 1tb = $32000 ==> " + contador7 + "\n8. Disco Solido ADATA 120gb = $9700 ==> " + contador8 + "\n9. Disco Solido Kingston 240gb = $13000 ==> " + contador9 + "\nUsted tiene agregados " + contarProductos() + " productos al carrito sumando un total de $" + sumarTotal());
 }
 
 function terminarCompra() {
@@ -121,16 +146,24 @@ function carrito(numero) {
 }
 
 // VARIABLES
-const producto1 = 1000;
-const producto2 = 500;
-const producto3 = 2000;
-const producto4 = 750;
-const producto5 = 1225;
+const producto1 = 42000;
+const producto2 = 13500;
+const producto3 = 43500;
+const producto4 = 12500;
+const producto5 = 160000;
+const producto6 = 165000;
+const producto7 = 32000;
+const producto8 = 9700;
+const producto9 = 1300;
 let contador1 = 0;
 let contador2 = 0;
 let contador3 = 0;
 let contador4 = 0;
 let contador5 = 0;
+let contador6 = 0;
+let contador7 = 0;
+let contador8 = 0;
+let contador9 = 0;
 
 // INICIO DEL PROGRAMA
 let numero = parseInt(prompt("Ingrese la operación deseada: \n1. Agregar al Carrito\n2. Quitar del Carrito\n3. Consultar Montos\n0. Terminar compra"));
